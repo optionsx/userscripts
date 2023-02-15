081ca9744087a43c7c26af1229d45fda625d8d64
optionsx <fake@protonmail.com>
2023-02-15 23:17:47 +0300

    w

--- bingCollector.user.js
+++ bingCollector.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         BingCollector
-// @version      1.2.3
+// @version      1.2.4
 // @namespace    optionsx
 // @description  get bingchat faster, earn points everyday
 // @author       github.com/optionsx
@@ -16,17 +16,23 @@
   try {
     let maxSearch;
     let searchCount;
-    const source = await fetch("https://www.bing.com/rewardsapp/flyout").then(
-      (res) => res?.text()
-    );
+    const source = await fetch(
+      "https://www.bing.com/rewardsapp/flyoutdata?channel=BingFlyout"
+    ).then((res) => res?.json());
+
+    if (
+      // not logged in or unsupported country
+      source.IsAuthenticated !== true &&
+      source.IsRewardsUser !== true &&
+      source.IsUnsupportedCountry === false
+    )
+      return;
+
     // regex to get max search count && current search count in the source :)
-    const maxRegex = /"max":"(\d+)"/;
-    const progressRegex = /"progress":"(\d+)"/;
-    const maxMatch = source.match(maxRegex);
-    const progressMatch = source.match(progressRegex);
     if (maxMatch && progressMatch) {
-      maxSearch = maxMatch[1];
-      searchCount = progressMatch[1];
+      maxSearch =
+        source.FlyoutResult.UserStatus.Counters.PCSearch[0].PointProgress;
+      searchCount = FlyoutResult.UserStatus.Counters.PCSearch[0].PointProgress;
     }
     // fetch random questions
     while (++searchCount < maxSearch) {
