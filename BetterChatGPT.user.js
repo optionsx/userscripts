dd052516a08f634a73147ab4627490546cb0f31d
optionsx <fake@protonmail.com>
2023-02-15 13:13:27 +0300

    Better

--- BetterChatGPT.user.js
+++ BetterChatGPT.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name           BetterChatGPT
 // @namespace      https://github.com/optionsx
-// @version        1.0.3
+// @version        1.0.4
 // @author         https://github.com/optionsx
 // @description    detects if chatgpt needs refresh, access to chatgpt while down, favicon based on status
-// @grant          GM.xmlHttpRequest
+// @grant          GM_setClipboard
+// @run-at         document-idle
 // @match          https://chat.openai.com/*
 // @icon           https://www.google.com/s2/favicons?sz=64&domain=openai.com
 // @updateURL      https://github.com/optionsx/userscripts/raw/master/BetterChatGPT.user.js
@@ -13,27 +14,29 @@
 // ==/UserScript==
 // check out TheTerrasque extension: https://github.com/TheTerrasque/chatgpt-firefox-extension
 
+
+let accessToken = "";
+document.addEventListener('keydown', function (event) {
+  if (event.key === ' ' && event.ctrlKey) {
+    GM_setClipboard(accessToken, alert("accessToken Copied to clipboard!"));
+  }
+});
 // access while down functionality
-const timedLoop = setInterval(() => {
-  if (document.getElementsByClassName("text-3xl font-medium").length > 0) location.reload();
-  else clearInterval(timedLoop);
-}, 500);
+if (document.getElementsByClassName("text-3xl font-medium").length > 0) location.reload();
 
 // sessionExpired? logout? functionality
 (async () => {
   try {
-    const resp = await fetch("https://chat.openai.com/api/auth/session", {
-      method: "GET",
-    }).then((res) => {
-      return res.json();
-    });
-    console.log(resp);
-    if (resp?.error === "RefreshAccessTokenError") {
-      changeFavicon("red");
-      alert("Session Expired. Logging out...");
-      localStorage.removeItem("__Secure-next-auth.session-token");
-      location.reload();
+    const resp = await fetch("https://chat.openai.com/api/auth/session");
+    console.log(resp.status === 403 ? "Session Expired" : "Session Active");
+    if (resp?.error !== "RefreshAccessTokenError") {
+      accessToken = await resp?.json().then(data => data?.accessToken);
+      return true
     }
+    changeFavicon("red");
+    alert("Session Expired. Logging out...");
+    localStorage.removeItem("__Secure-next-auth.session-token");
+    location.reload();
   } catch (e) {
     console.log(e);
   }
@@ -44,15 +47,14 @@ const loophole = setInterval(() => {
   try {
     if (document.querySelector(".toast-root").dataset.state) {
       changeFavicon("yellow");
-      document.querySelector(".toast-root").dataset.state = "exiting";
-      console.log("toast Now", document.querySelector(".toast-root").dataset.state)
-      if (document.querySelector(".toast-root") === null) clearInterval(loophole);
+      document.querySelector(".toast-root").innerHTML = "";
+      if (document.querySelector(".toast-root").innerHTML === "") clearInterval(loophole);
     }
   } catch { }
 }, 0);
 
 
-// reload if needed functionality
+// reload if needed functionality && copy to clipboard
 const triggerPoint = document.querySelector("textarea"); // "button.absolute" for submit button
 triggerPoint.addEventListener("click", detectReload);
 triggerPoint.addEventListener("mousemove", () => triggerPoint.focus());
@@ -60,13 +62,23 @@ async function detectReload() {
   try {
     const resp = await fetch("https://chat.openai.com/api/auth/session")
     console.log(resp?.status === 403 ? "reload" : "no reload");
-    // copy to clipboard triggerPoint.value
-    if (resp?.status !== 403) return
-    copy(document.querySelector("textarea").value);
+    if (resp?.status !== 403) return "no reload"
+    const prompt = document.querySelector("textarea").value;
     changeFavicon("yellow");
+    prompt.length > 0 ? localStorage.setItem('prompt', prompt) : null;
     location.reload();
   } catch { }
 }
+
+// cache prompt functionality
+setTimeout(() => {
+  const prompt = localStorage.getItem('prompt');
+  if (prompt) {
+    document.querySelector("textarea").value = prompt
+    localStorage.removeItem('prompt');
+    document.querySelector("textarea").focus()
+  }
+}, 500);
 
 const status = {
   yellow:
@@ -89,8 +101,8 @@ const changeFavicon = color => {
 const color = ["green", "black", "white", "yellow"];
 setInterval(() => {
   try {
-
     const randomColor = color[Math.floor(Math.random() * color.length)];
+    if (document.querySelector(".text-4xl").innerHTML !== `<a href="https://t.me/NimDev" class="text-4xl font-semibold text-center mt-6 sm:mt-[20vh] ml-auto mr-auto mb-10 sm:mb-16 flex gap-2 items-center justify-center">ChatGPT</a>`) document.querySelector(".text-4xl").innerHTML = `<a href="https://t.me/NimDev" class="text-4xl font-semibold text-center mt-6 sm:mt-[20vh] ml-auto mr-auto mb-10 sm:mb-16 flex gap-2 items-center justify-center">ChatGPT</a>`
     addCss(`.text-4xl { color: ${randomColor} !important; }`) // nice color right?
     if (document.querySelector("div.px-3").childNodes.length > 1) document.querySelector("div.px-3").childNodes[1].remove() // remove blah blah blah text which don't exist in plus version
     if (document.querySelector(".text-gray-800").childElementCount > 1) document.querySelector(".text-gray-800").lastChild.remove() // unnecessary block of text
