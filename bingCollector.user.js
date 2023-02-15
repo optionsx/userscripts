47b857b61db302132038e737ca253ac5a2ad484b
optionsx <fake@protonmail.com>
2023-02-15 13:55:43 +0300

    fff

--- bingCollector.user.js
+++ bingCollector.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         BingCollector
-// @version      1.0
+// @version      1.1
 // @namespace    optionsx
 // @description  get bingchat faster, earn points everyday
 // @author       github.com/optionsx
@@ -15,48 +15,47 @@
 // @license MIT
 // ==/UserScript==
 
-alert("tessssssssssssssss1212121")
-// (async () => {
-//     const day = new Date().getDay();
-//     if (await GM.getValue("count") === undefined) await GM.setValue("count", 0);
-//     if (await GM.getValue("day") === undefined) await GM.setValue("day", day);
-//     while (await GM.getValue("count") < 30 && await GM.getValue("day") === day) {
-//         const count = await GM.getValue("count");
-//         await GM.setValue("count", count + 1);
+(async () => {
+    const day = new Date().getDay();
+    if (await GM.getValue("count") === undefined) await GM.setValue("count", 0);
+    if (await GM.getValue("day") === undefined) await GM.setValue("day", day);
+    while (await GM.getValue("count") < 30 && await GM.getValue("day") < day) {
+        const count = await GM.getValue("count");
+        await GM.setValue("count", count + 1);
 
-//         const question = await fetch("https://www.boredapi.com/api/activity").then(
-//             (res) => res.json(),
-//         ).then((res) => res.activity);
+        const question = await fetch("https://www.boredapi.com/api/activity").then(
+            (res) => res.json(),
+        ).then((res) => res.activity);
 
-//         let url = `https://www.bing.com/search?q=${question}`;
-//         let newTab = GM_openInTab(url, { active: false, setParent: true }) ?? GM.openInTab(url, { active: false, setParent: true })
+        let url = `https://www.bing.com/search?q=${question}`;
+        let newTab = GM_openInTab(url, { active: false, setParent: true }) ?? GM.openInTab(url, { active: false, setParent: true })
 
-//         if (await GM.getValue("count") === 30) {
+        if (await GM.getValue("count") === 30) {
 
-//             if (day === 6) await GM.setValue("day", 0);
-//             else await GM.setValue("day", day + 1);
-//             await GM.setValue("count", 0);
-//         }
+            if (day === 6) await GM.setValue("day", 0);
+            else await GM.setValue("day", day + 1);
+            await GM.setValue("count", 0);
+        }
 
-//         await new Promise((resolve) => setTimeout(resolve, 1500));
-//         newTab.close();
-//     }
-//     // collect bonuses in drop - down menu
-//     if (await GM.getValue("count") === 0 && await GM.getValue("day") !== day) {
-//         try {
-//             while (document.getElementById("id_rh").attributes['aria-expanded'].textContent !== "true") {
-//                 document.getElementById("id_rh").click();
-//                 await new Promise(resolve => setTimeout(resolve, 3000));
-//                 for (let i = 0; i < document.getElementsByClassName("l_s l_s_b")[0].attributes[0].ownerDocument.links.length; i++) {
-//                     const link = document.getElementsByClassName("l_s l_s_b")[0].attributes[0].ownerDocument.links[i].href;
-//                     let newTab = window.open(link);
-//                     await new Promise(resolve => setTimeout(resolve, 1000));
-//                     newTab.close();
+        await new Promise((resolve) => setTimeout(resolve, 1500));
+        newTab.close();
+    }
+    // collect bonuses in drop - down menu
+    if (await GM.getValue("count") === 0 && await GM.getValue("day") !== day) {
+        try {
+            while (document.getElementById("id_rh").attributes['aria-expanded'].textContent !== "true") {
+                document.getElementById("id_rh").click();
+                await new Promise(resolve => setTimeout(resolve, 3000));
+                for (let i = 0; i < document.getElementsByClassName("l_s l_s_b")[0].attributes[0].ownerDocument.links.length; i++) {
+                    const link = document.getElementsByClassName("l_s l_s_b")[0].attributes[0].ownerDocument.links[i].href;
+                    let newTab = window.open(link);
+                    await new Promise(resolve => setTimeout(resolve, 1000));
+                    newTab.close();
 
-//                 }
-//             }
-//         } catch (e) { console.log(e) }
-//     }
+                }
+            }
+        } catch (e) { console.log(e) }
+    }
 
     // if (await GM.getValue("count") === 0 && await GM.getValue("day") !== day) {
     //     const source = await fetch(
