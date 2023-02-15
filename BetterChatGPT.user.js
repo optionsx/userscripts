9a64787046ee88dfd3c87d6b89f0aea0664dd8ab
optionsx <fake@protonmail.com>
2023-02-15 18:43:09 +0300

    conditionFix

--- BetterChatGPT.user.js
+++ BetterChatGPT.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           BetterChatGPT
 // @namespace      https://github.com/optionsx
-// @version        1.0.4
+// @version        1.0.5
 // @author         https://github.com/optionsx
 // @description    detects if chatgpt needs refresh, access to chatgpt while down, favicon based on status
 // @grant          GM_setClipboard
@@ -14,15 +14,15 @@
 // ==/UserScript==
 // check out TheTerrasque extension: https://github.com/TheTerrasque/chatgpt-firefox-extension
 
-
 let accessToken = "";
-document.addEventListener('keydown', function (event) {
-  if (event.key === ' ' && event.ctrlKey) {
+document.addEventListener("keydown", function (event) {
+  if (event.key === " " && event.ctrlKey) {
     GM_setClipboard(accessToken, alert("accessToken Copied to clipboard!"));
   }
 });
 // access while down functionality
-if (document.getElementsByClassName("text-3xl font-medium").length > 0) location.reload();
+if (document.getElementsByClassName("text-3xl font-medium").length > 0)
+  location.reload();
 
 // sessionExpired? logout? functionality
 (async () => {
@@ -30,8 +30,8 @@ if (document.getElementsByClassName("text-3xl font-medium").length > 0) location
     const resp = await fetch("https://chat.openai.com/api/auth/session");
     console.log(resp.status === 403 ? "Session Expired" : "Session Active");
     if (resp?.error !== "RefreshAccessTokenError") {
-      accessToken = await resp?.json().then(data => data?.accessToken);
-      return true
+      accessToken = await resp?.json().then((data) => data?.accessToken);
+      return true;
     }
     changeFavicon("red");
     alert("Session Expired. Logging out...");
@@ -48,11 +48,11 @@ const loophole = setInterval(() => {
     if (document.querySelector(".toast-root").dataset.state) {
       changeFavicon("yellow");
       document.querySelector(".toast-root").innerHTML = "";
-      if (document.querySelector(".toast-root").innerHTML === "") clearInterval(loophole);
+      if (document.querySelector(".toast-root").innerHTML === "")
+        clearInterval(loophole);
     }
-  } catch { }
+  } catch {}
 }, 0);
-
 
 // reload if needed functionality && copy to clipboard
 const triggerPoint = document.querySelector("textarea"); // "button.absolute" for submit button
@@ -60,23 +60,23 @@ triggerPoint.addEventListener("click", detectReload);
 triggerPoint.addEventListener("mousemove", () => triggerPoint.focus());
 async function detectReload() {
   try {
-    const resp = await fetch("https://chat.openai.com/api/auth/session")
+    const resp = await fetch("https://chat.openai.com/api/auth/session");
     console.log(resp?.status === 403 ? "reload" : "no reload");
-    if (resp?.status !== 403) return "no reload"
+    if (resp?.status !== 403) return "no reload";
     const prompt = document.querySelector("textarea").value;
     changeFavicon("yellow");
-    prompt.length > 0 ? localStorage.setItem('prompt', prompt) : null;
+    prompt.length > 0 ? localStorage.setItem("prompt", prompt) : null;
     location.reload();
-  } catch { }
+  } catch {}
 }
 
 // cache prompt functionality
 setTimeout(() => {
-  const prompt = localStorage.getItem('prompt');
+  const prompt = localStorage.getItem("prompt");
   if (prompt) {
-    document.querySelector("textarea").value = prompt
-    localStorage.removeItem('prompt');
-    document.querySelector("textarea").focus()
+    document.querySelector("textarea").value = prompt;
+    localStorage.removeItem("prompt");
+    document.querySelector("textarea").focus();
   }
 }, 500);
 
@@ -90,7 +90,7 @@ const status = {
 };
 
 // change favicon color functionality
-const changeFavicon = color => {
+const changeFavicon = (color) => {
   const icons = document.querySelectorAll("head link[rel='icon']");
   for (let i = 0; i < icons.length; i++) {
     icons[i].href = status[color];
@@ -102,17 +102,27 @@ const color = ["green", "black", "white", "yellow"];
 setInterval(() => {
   try {
     const randomColor = color[Math.floor(Math.random() * color.length)];
-    if (document.querySelector(".text-4xl").innerHTML !== `<a href="https://t.me/NimDev" class="text-4xl font-semibold text-center mt-6 sm:mt-[20vh] ml-auto mr-auto mb-10 sm:mb-16 flex gap-2 items-center justify-center">ChatGPT</a>`) document.querySelector(".text-4xl").innerHTML = `<a href="https://t.me/NimDev" class="text-4xl font-semibold text-center mt-6 sm:mt-[20vh] ml-auto mr-auto mb-10 sm:mb-16 flex gap-2 items-center justify-center">ChatGPT</a>`
-    addCss(`.text-4xl { color: ${randomColor} !important; }`) // nice color right?
-    if (document.querySelector("div.px-3").childNodes.length > 1) document.querySelector("div.px-3").childNodes[1].remove() // remove blah blah blah text which don't exist in plus version
-    if (document.querySelector(".text-gray-800").childElementCount > 1) document.querySelector(".text-gray-800").lastChild.remove() // unnecessary block of text
+    if (
+      document.querySelector(".text-4xl")?.innerHTML &&
+      document.querySelector(".text-4xl")?.innerHTML !==
+        `<a href="https://t.me/NimDev" class="text-4xl font-semibold text-center mt-6 sm:mt-[20vh] ml-auto mr-auto mb-10 sm:mb-16 flex gap-2 items-center justify-center">ChatGPT</a>`
+    )
+      document.querySelector(
+        ".text-4xl"
+      ).innerHTML = `<a href="https://t.me/NimDev" class="text-4xl font-semibold text-center mt-6 sm:mt-[20vh] ml-auto mr-auto mb-10 sm:mb-16 flex gap-2 items-center justify-center">ChatGPT</a>`;
+    addCss(`.text-4xl { color: ${randomColor} !important; }`); // nice color right?
+    if (document.querySelector("div.px-3").childNodes.length > 1)
+      document.querySelector("div.px-3").childNodes[1].remove(); // remove blah blah blah text which don't exist in plus version
+    if (document.querySelector(".text-gray-800").childElementCount > 1)
+      document.querySelector(".text-gray-800").lastChild.remove(); // unnecessary block of text
     //if (document.querySelector(".text-4xl").textContent === "ChatGPT") document.querySelector(".text-4xl").textContent = 'ChadGPT' // xD
+  } catch (e) {
+    console.log(e);
   }
-  catch (e) { console.log(e); }
 }, 1500);
 function addCss(cssString) {
-  var head = document.getElementsByTagName('head')[0];
-  var newCss = document.createElement('style');
+  var head = document.getElementsByTagName("head")[0];
+  var newCss = document.createElement("style");
   newCss.type = "text/css";
   newCss.innerHTML = cssString;
   head.appendChild(newCss);
