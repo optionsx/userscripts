7970a84ce3c926a6d1cd3b5fcc4436ffdb0dcbc5
optionsx <fake@protonmail.com>
2023-02-15 18:43:18 +0300

    F

--- bingCollector.user.js
+++ bingCollector.user.js
@@ -16,70 +16,57 @@
 // ==/UserScript==
 
 (async () => {
-    const day = new Date().getDay();
-    if (await GM.getValue("count") === undefined) await GM.setValue("count", 0);
-    if (await GM.getValue("day") === undefined) await GM.setValue("day", day);
-    while (await GM.getValue("count") < 30 && await GM.getValue("day") < day) {
-        const count = await GM.getValue("count");
-        await GM.setValue("count", count + 1);
-
-        const question = await fetch("https://www.boredapi.com/api/activity").then(
-            (res) => res.json(),
-        ).then((res) => res.activity);
-
-        let url = `https://www.bing.com/search?q=${question}`;
-        let newTab = GM_openInTab(url, { active: false, setParent: true }) ?? GM.openInTab(url, { active: false, setParent: true })
-
-        if (await GM.getValue("count") === 30) {
-
-            if (day === 6) await GM.setValue("day", 0);
-            else await GM.setValue("day", day + 1);
-            await GM.setValue("count", 0);
-        }
-
-        await new Promise((resolve) => setTimeout(resolve, 1500));
-        newTab.close();
-    }
-    // collect bonuses in drop - down menu
-    if (await GM.getValue("count") === 0 && await GM.getValue("day") !== day) {
-        try {
-            while (document.getElementById("id_rh").attributes['aria-expanded'].textContent !== "true") {
-                document.getElementById("id_rh").click();
-                await new Promise(resolve => setTimeout(resolve, 3000));
-                for (let i = 0; i < document.getElementsByClassName("l_s l_s_b")[0].attributes[0].ownerDocument.links.length; i++) {
-                    const link = document.getElementsByClassName("l_s l_s_b")[0].attributes[0].ownerDocument.links[i].href;
-                    let newTab = window.open(link);
-                    await new Promise(resolve => setTimeout(resolve, 1000));
-                    newTab.close();
-
-                }
-            }
-        } catch (e) { console.log(e) }
-    }
-
-    // if (await GM.getValue("count") === 0 && await GM.getValue("day") !== day) {
-    //     const source = await fetch(
-    //         "https://www.bing.com/rewardsapp/flyoutdata?channel=BingFlyout&partnerId=BingRewards",
-    //     ).then(res => res?.json())
-    //     if (source.IsAuthenticated !== true && source.IsRewardsUser !== true && source.IsUnsupportedCountry === false) return; // not logged in or unsupported country
-    //     // itterate through urlReward bonuses
-    //     for (let i = 0; i < source.FlyoutResult.MorePromotions.length; i++) {
-    //         const el = source.FlyoutResult.MorePromotions[i];
-    //         if (el.Complete !== true && el.IsRewardable === true) {
-    //             const newTab = GM_openInTab(el.DestinationUrl, { active: false, setParent: true }) ?? GM.openInTab(el.DestinationUrl, { active: false, setParent: true })
-    //             await new Promise((resolve) => setTimeout(resolve, 1500));
-    //             newTab.close();
-    //         }
-    //     }
-
-    // itterate through search bonuses
-    // for (let i = 0; i < source.FlyoutResult.UserStatus.Counters['PCSearch'].length; i++) {
-    //     const el = source.FlyoutResult.UserStatus.Counters['PCSearch'][i];
-    //     if (el.Complete !== true && el.IsRewardable === true && el.PromotionType === "Search") {
-    //         const newTab = GM_openInTab(el.DestinationUrl, { active: false, setParent: true }) ?? GM.openInTab(el.DestinationUrl, { active: false, setParent: true })
-    //         await new Promise((resolve) => setTimeout(resolve, 1500));
-    //         newTab.close();
-    //     }
-    // }
-
+  const day = Date.now();
+  let maxSearch;
+  let searchCount;
+  if ((await GM.getValue("count")) === undefined)
+    await GM.setValue("searchCount", 0);
+  if ((await GM.getValue("day")) === undefined) await GM.setValue("day", day);
+  const source = await fetch("https://www.bing.com/rewardsapp/flyout").then(
+    (res) => res?.text()
+  );
+  // regex to get max search count && current search count
+  const maxRegex = /"max":"(\d+)"/;
+  const progressRegex = /"progress":"(\d+)"/;
+  const maxMatch = source.match(maxRegex);
+  const progressMatch = source.match(progressRegex);
+  if (maxMatch && progressMatch) {
+    maxSearch = maxMatch[1];
+    searchCount = progressMatch[1];
+  }
+  // fetch random questions
+  while (++searchCount < maxSearch) {
+    const question = await fetch("https://www.boredapi.com/api/activity")
+      .then((res) => res.json())
+      .then((res) => res.activity);
+    let url = `https://www.bing.com/search?q=${question}`;
+    let newTab =
+      GM_openInTab(url, { active: false, setParent: true }) ??
+      GM.openInTab(url, { active: false, setParent: true });
+  }
 })();
+
+// if (await GM.getValue("count") === 0 && await GM.getValue("day") !== day) {
+//     const source = await fetch(
+//         "https://www.bing.com/rewardsapp/flyoutdata?channel=BingFlyout&partnerId=BingRewards",
+//     ).then(res => res?.json())
+//     if (source.IsAuthenticated !== true && source.IsRewardsUser !== true && source.IsUnsupportedCountry === false) return; // not logged in or unsupported country
+//     // itterate through urlReward bonuses
+//     for (let i = 0; i < source.FlyoutResult.MorePromotions.length; i++) {
+//         const el = source.FlyoutResult.MorePromotions[i];
+//         if (el.Complete !== true && el.IsRewardable === true) {
+//             const newTab = GM_openInTab(el.DestinationUrl, { active: false, setParent: true }) ?? GM.openInTab(el.DestinationUrl, { active: false, setParent: true })
+//             await new Promise((resolve) => setTimeout(resolve, 1500));
+//             newTab.close();
+//         }
+//     }
+
+// itterate through search bonuses
+// for (let i = 0; i < source.FlyoutResult.UserStatus.Counters['PCSearch'].length; i++) {
+//     const el = source.FlyoutResult.UserStatus.Counters['PCSearch'][i];
+//     if (el.Complete !== true && el.IsRewardable === true && el.PromotionType === "Search") {
+//         const newTab = GM_openInTab(el.DestinationUrl, { active: false, setParent: true }) ?? GM.openInTab(el.DestinationUrl, { active: false, setParent: true })
+//         await new Promise((resolve) => setTimeout(resolve, 1500));
+//         newTab.close();
+//     }
+// }
