53be3d5ae5bfb1893725e5ff3917cc34535492d4
optionsx <52641788+optionsx@users.noreply.github.com>
2023-05-20 12:03:05 +0300

    patch v0.2

--- HighQualityOnly.user.js
+++ HighQualityOnly.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         HighQualityOnly
-// @version      0.1
+// @version      0.2
 // @description  selects The highest quality in Youtube
 // @author       chzu
 // @run-at       document-idle
@@ -8,8 +8,11 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
-const player = document.querySelector('#movie_player, .html5-video-player');
-player?.setPlaybackQualityRange("highres")
-player?.setPlaybackQuality('highres');
-player?.setPlaybackQualityRange(player.getPlaybackQuality())
-player?.setPlaybackQuality(player.getPlaybackQuality());
+
+const player = document.querySelector("#movie_player, .html5-video-player");
+const quality = player.getAvailableQualityLevels();
+setInterval(_ => {
+    if (player.getPlaybackQuality === quality[0]) return;
+    player.setPlaybackQuality(quality[0]);
+    player.setPlaybackQualityRange(quality[0]);
+}, 5000);
